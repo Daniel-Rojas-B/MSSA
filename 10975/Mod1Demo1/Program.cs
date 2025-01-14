1ea5ceefec54b8e14f1bc3d1105af4ee08ee1412
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2025-01-13 16:35:32 -0800

    Week 1 Day 1

---
+++ 10975/Mod1Demo1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod1Demo1
+{
+    // marks scope
+    internal class Program
+    {
+        // Starting point
+        static void Main(string[] args)
+        {   // main starts
+
+
+            int num = 10;
+            Console.WriteLine(num);
+            Console.ForegroundColor = ConsoleColor.Green; // asssigning color
+            Console.WriteLine("Hello world!");
+            Console.ReadKey(); // read a character
+        }   // main ends
+    }
+}
---
+++ 10975/Mod2Demo1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod2Demo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Variables and data types demo");
+
+            // data type and variable name
+            int i; //32 bit signed
+            char c='n'; // initializing
+            float rateOfInterest = 2.4f;
+            string name;
+            string address;
+            float age;
+            Console.WriteLine("Enter your name:");
+            name = Console.ReadLine();
+            Console.WriteLine(name + ", Where do you live?:");
+            address=Console.ReadLine();
+
+            Console.WriteLine("Oh, you live in " + address + ". How old are you?");
+            age = Convert.ToSingle(Console.ReadLine());
+
+            int num1, num2;
+            Console.WriteLine("Enter num1 and num2");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            int result = num1 + num2;
+            Console.WriteLine("The sum is: " + result);
+            float divResult = num1 / num2;
+            float remainder = num1 % num2;
+            Console.WriteLine(divResult);
+            Console.WriteLine(remainder);
+
+            Console.ReadKey();
+        }
+    }
+}
