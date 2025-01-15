580204d8b1ff2b8ce793f424cb9cb2ad06564e97
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2025-01-14 18:14:21 -0800

    Added week 1 day 2

---
+++ 10975/Mod3Functionsdemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod3Functionsdemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Please enter your name:");
+            name = Console.ReadLine();
+            DisplayGreeting(name); // calling the method with a string parameter
+            int num1, num2;
+            Console.WriteLine("Enter 2 numbers to add: ");
+            num1 = Int32.Parse(Console.ReadLine());
+            num2 = Int32.Parse(Console.ReadLine());
+            int result = AddNumbers(num1, num2);
+            Console.WriteLine($"The sum of {num1} and {num2} is {result}");
+            Console.ReadKey();
+        }
+
+        static void DisplayGreeting(string nameOfPerson) // list of parameters
+        {
+            Console.WriteLine("Welcome " + nameOfPerson); // concatenation
+            Console.WriteLine($"Hi {nameOfPerson}, How are you?"); // interpolation
+        }
+        static int AddNumbers(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ 10975/Mod3IfElseStatements/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Mod3IfElseStatements")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mod3IfElseStatements")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ea3cc5c8-3b7f-4e01-b688-bd4dc351dcb3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
