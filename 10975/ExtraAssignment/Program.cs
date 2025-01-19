79840618cffb69f00e846e89fb5a27a4ee21d205
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2025-01-19 00:32:01 -0800

    Added extra assignment

--- 10975/ExtraAssignment/Program.cs
+++ 10975/ExtraAssignment/Program.cs
@@ -13,6 +13,10 @@ namespace ExtraAssignment
             Console.WriteLine("Input the number(Table to be calculated):");
             int number = Int32.Parse(Console.ReadLine());
             multiplicationTable(number);
+
+            Console.WriteLine("Input number of terms");
+            int terms = Int32.Parse(Console.ReadLine());
+            oddNaturalNumbers(terms);
         }    
         static void multiplicationTable(int number)
         {
@@ -20,6 +24,22 @@ namespace ExtraAssignment
             {
                 Console.WriteLine(number + "X" + i + " = " + i * number);
             }
+        }
+        static void oddNaturalNumbers(int terms)
+        {
+            int sum = 0;
+            int numberOdd = 0;
+            Console.WriteLine("The odd numbers are:");
+            for (int i = 1; i <= terms; i++)
+            {
+
+                Console.Write(numberOdd + 1 + " ");
+                sum += numberOdd + 1;
+                numberOdd = numberOdd + 2;
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine("The sum of odd numbers are:" + sum);
         }
 
     }
