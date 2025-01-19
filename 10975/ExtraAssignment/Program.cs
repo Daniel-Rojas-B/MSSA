721fd8b3e7d08f83049e1b4a5f4935cdbd98e76e
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2025-01-18 23:36:48 -0800

    Added extra assignment

---
+++ 10975/ExtraAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the number(Table to be calculated):");
+            int number = Int32.Parse(Console.ReadLine());
+            multiplicationTable(number);
+        }    
+        static void multiplicationTable(int number)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(number + "X" + i + " = " + i * number);
+            }
+        }
+
+    }
+}
