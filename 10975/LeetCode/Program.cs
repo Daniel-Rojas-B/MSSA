f9dd6a00c753635ef4be85e0a18e62b92cb41c3c
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2025-01-26 23:23:03 -0800

    Added leet code exercise

--- 10975/LeetCode/Program.cs
+++ 10975/LeetCode/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,13 +13,16 @@ namespace LeetCode
         {
             int[] result = TwoSum(new int[] { 3,2,4 }, 6);
             Console.WriteLine($"[{result[0]}, {result[1]}]");
+
+            List<int> result2 = RemoveDuplicates(new List<int> { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 });
+            Console.WriteLine(result2);
         }
         public static int[] TwoSum(int[] nums, int target)
         {
             int[] indexArray = new int[2];
             for (int i = 0; i < nums.Length - 1; i++)
             {
-                for (int j = 1; j < nums.Length; j++)
+                for (int j = i + 1; j < nums.Length; j++)
                 {
                     if (nums[i] + nums[j] == target)
                     {
@@ -28,6 +32,18 @@ namespace LeetCode
                 }
             }
             return indexArray;
+        }
+        public static List<int> RemoveDuplicates(List<int> nums)
+        {
+            List<int> numsList = new List<int> ();
+            for (int i = 1; i < nums.Count; i++)
+            {                
+                    if ( nums[i] != nums[i-1])
+                    {
+                    numsList.Add(nums[i]);
+                    }                
+            }
+            return numsList;
         }
     }
 
