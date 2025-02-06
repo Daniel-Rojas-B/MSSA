ea3e84674c17e58625051ad4fa2643a8905c2fd3
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2025-02-06 12:13:40 -0800

    Added week 4 day 4

---
+++ 20483/ExtraAssignments_week4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ExtraAssignments_week4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ---- Problem 1 ---- ");
+            Console.WriteLine(Contains3(7201432));
+            Console.WriteLine(Contains3(87501));
+
+            Console.ReadKey();
+        }
+        public static bool Contains3(int number)
+        {          
+            while (number > 0)
+            {
+                if (number % 10 == 3)
+                {
+                    return true;
+                }
+                number = number / 10;
+            }
+            return false;
+        }
+    }
+}
