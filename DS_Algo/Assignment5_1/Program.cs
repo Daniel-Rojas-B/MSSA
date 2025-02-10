bc2631e3a8326810b8bc8f34a26cf73c947b5078
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2025-02-10 15:17:42 -0800

    Added week 5 day 1

---
+++ DS_Algo/Assignment5_1/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Assignment5_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ---- Problem 1 ---- ");
+            Console.WriteLine(IsPalindrome(121));
+
+            Console.WriteLine(" ---- Problem 2 ---- ");
+            Console.WriteLine(SumNumber(1234));
+
+            Console.WriteLine(" ---- Problem 3 ---- ");
+            Console.WriteLine(ArrayTwice(new int[] { 1, 2, 3, 1 }));
+            Console.WriteLine(ArrayTwice(new int[] { 1, 2, 3, 4 }));
+            Console.WriteLine(ArrayTwice(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }));
+
+        }
+        static bool IsPalindrome(int integer)
+        {
+            if (integer < 0)
+            {
+                return false;
+            }
+            int initialInteger = integer;
+            int comparisonNumber = 0;
+            while (integer > 0)
+            {
+                comparisonNumber = comparisonNumber*10 + integer % 10;
+                integer = integer / 10;
+            }
+            return initialInteger == comparisonNumber;
+        }
+        static int SumNumber(int number)
+        {
+            int sum = 0;
+            
+            while (number > 0)
+            {
+                sum += number % 10;
+                number = number / 10;
+            }
+            
+            return sum;
+        }
+        static bool ArrayTwice(int[] nums)
+        {
+            bool contains = true;
+            HashSet<int> set = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                set.Add(nums[i]);
+            }
+            if (nums.Length == set.Count)
+            {
+                contains = false;
+            }
+            return contains;
+
+        }
+    }
+}
---
+++ DS_Algo/Mod1HappyNumber/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Mod1HappyNumber
+{
+    internal class Program
+    {
+        
+        static bool Sumofdigitssquare(int n)
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            while (n > 0)
+            {
+
+                int sum = 0;
+                while (n > 0)
+                {
+                    int digit = n % 10;
+                    n = n / 10;
+                    sum = sum + digit * digit;
+                }
+            
+
+                if (set.Contains(sum))
+                {
+                    return false; // means its in a loop
+                }
+                else
+                {
+                    set.Add(sum);
+                }
+
+                if (sum == 1)
+                {
+                    return true;
+                }
+                n = sum;
+            }
+            return false;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sumofdigitssquare(19));
+            
+            Console.ReadKey();
+        }
+    }
+}
