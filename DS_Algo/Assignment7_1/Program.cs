dd72dcd759d4d21888b49573fe13d9289cd331e8
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2025-02-24 22:59:17 -0800

    Added week 7 day 1

---
+++ DS_Algo/Assignment7_1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+
+namespace Assignment7_1
+{
+    internal class Program
+    {
+        static void SelectionGradesSort(int[] Grades)
+        {
+            int minPosition = 0;
+            int temp = 0;
+            for (int i = 0; i < Grades.Length-1; i++)
+            {
+                minPosition = i;
+                for (int j = i+1; j < Grades.Length; j++)
+                {
+                    if (Grades[j] < Grades[minPosition])
+                    {
+                        minPosition = j;
+                    }
+                    // swap
+                    if (minPosition != i)
+                    {
+                        temp = Grades[i];
+                        Grades[i] = Grades[minPosition];
+                        Grades[minPosition]= temp;
+                    }
+                }
+            }
+        }
+
+        public static string MergeWords(string word1, string word2)
+        {
+            StringBuilder result = new StringBuilder();
+            int i = 0, j = 0;
+            while (i < word1.Length && j < word2.Length)
+            {
+                result.Append(word1[i]).Append(word2[j]);
+                i++;
+                j++;
+            }
+
+            while (i < word1.Length)
+            {
+                result.Append(word1[i]);
+                i++;
+            }
+
+            while (j < word2.Length)
+            {
+                result.Append(word2[j]);
+                j++;
+            }
+
+            return result.ToString();
+        }
+        static void Main(string[] args)
+        {
+            int[] Grades = { 70, 85, 69, 95, 90, 96, 71, 99, 84 };
+            SelectionGradesSort(Grades);
+            Console.WriteLine(" ---- Problem 1 ----");
+            Console.WriteLine(" ---- Sorted Grades ----");
+            foreach (int Grade in Grades)
+            {
+                Console.WriteLine(Grade);            }
+
+            Console.WriteLine(" ---- Problem 2 ----");
+            Console.WriteLine(MergeWords("abc", "pqr"));
+            Console.WriteLine(MergeWords("ab", "pqrs"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DS_Algo/Week6_challenge/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Week6_challenge
+{
+    internal class Program
+    {
+        static void RotateMatrix(int[][] matrix)
+        {
+            int n = matrix.Length;
+            
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {                    
+                    int temp = matrix[i][j];
+                    matrix[i][j] = matrix[j][i];
+                    matrix[j][i] = temp;
+                }
+            }
+            
+            for (int i = 0; i < n; i++)
+            {
+                Array.Reverse(matrix[i]);
+            }
+            foreach (var num in matrix)
+            {
+                Console.WriteLine(string.Join(" ", num));
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            RotateMatrix(new[] { new[] { 1, 2, 3 }, new[] { 4, 5, 6 }, new[] { 7, 8, 9 } });
+            RotateMatrix(new[] { new[] { 5,1,9,11 }, new[] { 2,4,8,10 }, new[] { 13,3,6,7 }, new[] { 15, 14, 12, 16 } });
+
+        }
+    }
+}
