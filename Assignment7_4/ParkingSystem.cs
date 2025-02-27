a9a9a4d8431822555db32dfc59e2788081b799a9
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2025-02-27 15:55:27 -0800

    Added week 7 day 4

---
+++ Assignment7_4/ParkingSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment7_4
+{
+    internal class ParkingSystem
+    {
+        private int[] slots;
+        public ParkingSystem(int big, int medium, int small)
+        {
+            this.slots = new int[] {0, big, medium, small};
+        }
+        public bool AddCar(int CarType)
+        {
+            if (this.slots[CarType]>0)
+            {
+                this.slots[CarType]--;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Assignment7_4/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Assignment7_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ---- Problem 1 ---- ");
+            ParkingSystem ps = new ParkingSystem(1,1,0);
+            Console.WriteLine(ps.AddCar(1));
+            Console.WriteLine(ps.AddCar(2));
+            Console.WriteLine(ps.AddCar(3));
+            Console.WriteLine(ps.AddCar(1));
+        }
+    }
+}
