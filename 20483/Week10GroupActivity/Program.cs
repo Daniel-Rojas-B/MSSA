2fcff12ceb3a06ccb0b91f67fc221514ecc76109
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2025-03-20 13:29:02 -0700

    Added week 10 day 4

---
+++ 20483/Week10GroupActivity/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Week10GroupActivity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ---- Group challenge ---- ");
+
+            int[] nums1 = { 100, 4, 200, 1, 3, 2 };
+            Console.WriteLine(Sequence(nums1)); 
+
+            int[] nums2 = { 0, 3, 7, 2, 5, 8, 6, 4, 0, 1 };
+            Console.WriteLine(Sequence(nums2)); 
+
+        }
+        public static int Sequence(int[] nums)
+        {
+            HashSet<int> uniqueNumbers = new HashSet<int>(nums);
+            // 0, 3, 7, 2, 5, 8, 6, 4, 1    take out the second 0
+            int counter =0;
+
+            foreach (int num in uniqueNumbers)
+            {
+                if (!uniqueNumbers.Contains(num - 1))
+                {
+                    int currentNum = num;
+                    int startPoint = 1;
+
+                    while (uniqueNumbers.Contains(currentNum + 1))
+                    {
+                        currentNum++;
+                        startPoint++;
+                    }
+
+                    counter = Math.Max(counter, startPoint);
+                }
+            }
+            return counter;
+        }
+    }
+}
