f355bc3fa61cb93e80a5ba1c24d004fc33dce255
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2025-03-31 12:33:03 -0700

    Added week 12 day 1

---
+++ 20483/Assignment12_1/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Assignment12_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---- Problem 1 ----");
+            Console.WriteLine(Strings("abcc", "oweabc"));
+
+        }
+        public static bool Strings(string ransomNote, string magazine)
+        {
+            bool output = true;
+
+            Dictionary<char, int> ransomNoteDict = new Dictionary<char, int>();
+            for (int i = 0; i < ransomNote.Length; i++)
+            {
+                if (!ransomNoteDict.ContainsKey(ransomNote[i]))
+                {
+                    ransomNoteDict[ransomNote[i]] = 0;
+                    ransomNoteDict[ransomNote[i]]++;
+                }
+                else
+                {
+                    ransomNoteDict[ransomNote[i]]++;
+                }
+            }
+            Dictionary<char, int> magazineDict = new Dictionary<char, int>();
+            for (int i = 0; i < magazine.Length; i++)
+            {
+                if (!magazineDict.ContainsKey(magazine[i]))
+                {
+                    magazineDict[magazine[i]] = 0;
+                    magazineDict[magazine[i]]++;
+                }
+                else
+                {
+                    magazineDict[magazine[i]]++;
+                }
+            }
+            foreach (var pair in ransomNoteDict)
+            {
+                char letter = pair.Key;
+                int frequencyNeeded = pair.Value;
+
+                if (!magazineDict.ContainsKey(letter) || magazineDict[letter] < frequencyNeeded)
+                {
+                    output = false;
+                    break;
+                }
+            }
+
+
+            return output;
+        }
+    }
+}
