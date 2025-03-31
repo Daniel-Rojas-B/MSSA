31805289e4c77dfcef2bbdcd5539fce0515bd470
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2025-03-30 23:37:34 -0700

    Added week 11 challenge lab

---
+++ 20483/Assignment11_3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace Assignment11_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ---- Problem 1 ---- ");
+            int[] numbers1 = new int[] { 40, 10, 20, 30 };
+            Rank(numbers1);
+            int[] numbers2 = new int[] { 100, 100, 100 };
+            Rank(numbers2);
+            int[] numbers3 = new int[] { 37, 12, 28, 9, 100, 56, 80, 5, 12 };
+            Rank(numbers3);
+
+            Console.WriteLine(" ---- Problem 2 ---- ");
+            int[] nums1 = new int[] { 3, 2, 3 };
+            Console.WriteLine(Majority(nums1));
+            int[] nums2 = new int[] { 2, 2, 1, 1, 1, 2, 2 };
+            Console.WriteLine(Majority(nums2));
+
+
+            Console.ReadLine();
+        }
+        public static void Rank(int[] arr)
+        {
+            int[] sortedArr = arr.ToArray();
+            for (int i =0; i< sortedArr.Length-1;i++)
+            {                 
+                for (int j = 0; j < sortedArr.Length - i - 1; j++)
+                {
+                    
+                    if (sortedArr[j] > sortedArr[j + 1])
+                    {
+                        int temp = sortedArr[j];
+                        sortedArr[j] = sortedArr[j + 1];
+                        sortedArr[j + 1] = temp;
+                    }
+                }
+            }
+
+            Dictionary<int, int> rankingDict = new Dictionary<int, int>();
+            int rank = 1;
+            for (int i = 0; i < sortedArr.Length; i++)
+            {
+                if (!rankingDict.ContainsKey(sortedArr[i])) 
+                {
+                    rankingDict[sortedArr[i]] = rank;
+                    rank++; 
+                }
+            }
+            int[] rankedArr = arr.Select(num => rankingDict[num]).ToArray();
+            Console.WriteLine(string.Join(", ", rankedArr));
+        }
+        public static int Majority(int[] nums)
+        {
+            int n = nums.Length;
+
+            Dictionary<int, int> frequencyDict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!frequencyDict.ContainsKey(nums[i]))
+                {
+                    frequencyDict[nums[i]] = 1;
+                    
+                }
+                else
+                {
+                    frequencyDict[nums[i]]++;
+                }
+            }
+            foreach (var pair in frequencyDict)
+            {
+                if (pair.Value > n/2)
+                {
+                    int majorityElement = pair.Value;
+                    return pair.Key;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ 20483/Week11_ChallengeLab/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Week11_ChallengeLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---- Problem 1 ----");
+            int[] nums1 = new int[] { 2, 0, 2, 1, 1, 0 };
+            Colors(nums1);
+            int[] nums2 = new int[] { 2,0,1 };
+            Colors(nums2);
+
+            Console.WriteLine("---- Problem 2 ----");
+            Balloons("nlaebolko");
+            Balloons("loonbalxballpoon");
+            Balloons("leetcode");
+
+        }
+        public static void Colors(int[] nums)
+        {
+            int low = 0;              
+            int mid = 0;              
+            int high = nums.Length - 1; 
+
+            while (mid <= high)
+            {
+                if (nums[mid] == 0)   
+                {                    
+                    int temp = nums[low];
+                    nums[low] = nums[mid];
+                    nums[mid] = temp;
+
+                    low++;
+                    mid++;
+                }
+                else if (nums[mid] == 1) 
+                {
+                    mid++;
+                }
+                else                    
+                {                    
+                    int temp = nums[mid];
+                    nums[mid] = nums[high];
+                    nums[high] = temp;
+
+                    high--;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", nums));
+        }
+        public static void Balloons(string text)
+        {
+            char[] keys = { 'b', 'a', 'l', 'l', 'o', 'o', 'n' };
+            Dictionary<char,int> balloonDict = new Dictionary<char,int>();
+
+            foreach (char key in keys)
+            {
+                if (!balloonDict.ContainsKey(key))
+                {
+                    balloonDict[key] = 0;
+                }
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if(balloonDict.ContainsKey(text[i]))
+                {
+                    balloonDict[text[i]]++;
+                }
+            }
+            int numBalloons = Math.Min(balloonDict['b'], Math.Min(balloonDict['a'],
+                            Math.Min(balloonDict['l'] / 2, Math.Min(balloonDict['o'] / 2, balloonDict['n']))));
+            Console.WriteLine(numBalloons);
+        }
+    }
+}
