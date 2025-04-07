ded7e118ae8debd7201d2af38e073764587a9f5f
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2025-04-06 23:59:25 -0700

    Added week 12 day 4

---
+++ 20483/Assignment12_3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignment12_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---- Problem 1----");
+            RemoveDuplicates("abbaca");
+        }
+        public static void RemoveDuplicates(string word)
+        {
+            for(int i  = 0; i < word.Length-1; i++)
+            {
+                if (word[i] == word[i + 1])
+                {
+                    word = word.Remove(i,2);
+                    i = -1;
+                }
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
